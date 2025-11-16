1677f902f796af7896c57ddcbe964ace9bf758f2
Rodrigo Rosa <rodsiqueirabh@hotmail.com>
2025-11-16 01:18:16 -0300

    Primeiro commit

---
+++ src/test/java/com/br/rodrigo/streamrodao/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.rodrigo.streamrodao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
