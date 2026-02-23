2303372d2ed848cbe67c3156300ff009ed7c57de
Rodrigo Rosa <rodsiqueirabh@hotmail.com>
2026-02-22 22:55:11 -0300

    Criando exceção especifica - Adocao controller

---
+++ src/main/java/com/br/rodrigo/streamrodao/domain3/exception/AdocaoException.java
@@ -0,0 +1,8 @@
+package com.br.rodrigo.streamrodao.domain3.exception;
+
+public class AdocaoException extends RuntimeException {
+
+    public AdocaoException(String mensagem) {
+        super(mensagem);
+    }
+}
--- src/main/java/com/br/rodrigo/streamrodao/domain3/service/AdocaoService.java
+++ src/main/java/com/br/rodrigo/streamrodao/domain3/service/AdocaoService.java
@@ -4,6 +4,7 @@ import com.br.rodrigo.streamrodao.domain3.dto.AdocaoDTO;
 import com.br.rodrigo.streamrodao.domain3.dto.AprovarAdocaoDTO;
 import com.br.rodrigo.streamrodao.domain3.dto.ReprovarAdocaoDTO;
 import com.br.rodrigo.streamrodao.domain3.dto.SolicitacaoDeAdocaoDTO;
+import com.br.rodrigo.streamrodao.domain3.exception.AdocaoException;
 import com.br.rodrigo.streamrodao.domain3.model.Adocao;
 import com.br.rodrigo.streamrodao.domain3.model.Pet;
 import com.br.rodrigo.streamrodao.domain3.model.StatusAdocao;
@@ -28,49 +29,50 @@ public class AdocaoService {
     @Autowired
     private AdocaoRepository adocaoRepository;
 
-    public List<AdocaoDTO> listarTodos(){
+    public List<AdocaoDTO> listarTodos() {
 
         return adocaoRepository.findAll().stream().map(AdocaoDTO::new).toList();
     }
-    public AdocaoDTO listar(Long id){
+
+    public AdocaoDTO listar(Long id) {
 
         return adocaoRepository.findById(id).stream().findFirst().map(AdocaoDTO::new).orElse(null);
     }
 
-    public void solicitar(SolicitacaoDeAdocaoDTO dto){
+    public void solicitar(SolicitacaoDeAdocaoDTO dto) {
         Pet pet = petRepository.getReferenceById(dto.idPet());
         Tutor tutor = tutorRepository.getReferenceById(dto.idTutor());
 
         //Pet já adotado;
-        if(pet.getAdotado()){
-            throw new IllegalStateException("Pet já adotado");
+        if (pet.getAdotado()) {
+            throw new AdocaoException("Pet já adotado");
         }
 
         //Pet com solicitação de adoção em andamento;
-        Boolean petAdocaoEmAndamento = adocaoRepository.existsByPetIdAndStatus(dto.idPet(),StatusAdocao.AGUARDANDO_AVALIACAO);
+        Boolean petAdocaoEmAndamento = adocaoRepository.existsByPetIdAndStatus(dto.idPet(), StatusAdocao.AGUARDANDO_AVALIACAO);
 
-        if(petAdocaoEmAndamento){
-            throw new UnsupportedOperationException("Pet com adocão em andamento");
+        if (petAdocaoEmAndamento) {
+            throw new AdocaoException("Pet com adocão em andamento");
         }
 
         //Tutor com 2 adoções aprovadas.
 
         Integer tutorAdocoes = adocaoRepository.countByTutorIdAndStatus(dto.idTutor(), StatusAdocao.APROVADO);
 
-        if (tutorAdocoes == 2){
-            throw new IllegalStateException("Tutor com máximo de adocoes");
+        if (tutorAdocoes == 2) {
+            throw new AdocaoException("Tutor com máximo de adocoes");
         }
 
-        adocaoRepository.save(new Adocao(tutor,pet, dto.motivo()));
+        adocaoRepository.save(new Adocao(tutor, pet, dto.motivo()));
     }
 
-    public void aprovar(AprovarAdocaoDTO dto){
+    public void aprovar(AprovarAdocaoDTO dto) {
         Adocao adocao = adocaoRepository.getReferenceById(dto.idAdocao());
         adocao.marcarComoAprovada();
         adocao.getPet().marcarComoAdotado();
     }
 
-    public void reprovar(ReprovarAdocaoDTO dto){
+    public void reprovar(ReprovarAdocaoDTO dto) {
         Adocao adocao = adocaoRepository.getReferenceById(dto.idAdocao());
         adocao.marcarComoReprovada(dto.justificativa());
     }
